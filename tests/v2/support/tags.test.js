208367bd43e92452e8be6e8869f51bbb759c03e5
androozka <androozka@gmail.com>
2019-08-22 23:53:54 -0700

    updated tests

--- tests/v2/support/tags.test.js
+++ tests/v2/support/tags.test.js
@@ -1,17 +1,18 @@
 const endpoint = require('../../../src/v2/support/tags');
 
 describe('Tags', () => {
+  const instance = 'instance';
   const headers = {};
   const data = {};
   let tags;
 
-  beforeAll(() => (tags = endpoint('', headers)));
+  beforeAll(() => (tags = endpoint(instance, headers)));
   afterAll(() => (tags = null));
 
   test('List Tags', () => {
     expect(tags.list()).toEqual({
       method: 'GET',
-      url: '/api/v2/tags.json',
+      url: `https://${instance}.zendesk.com/api/v2/tags.json`,
       headers
     });
   });
@@ -19,19 +20,19 @@ describe('Tags', () => {
   test('Show Tags', () => {
     expect(tags.show('tickets', 123)).toEqual({
       method: 'GET',
-      url: '/api/v2/tickets/123/tags.json',
+      url: `https://${instance}.zendesk.com/api/v2/tickets/123/tags.json`,
       headers
     });
 
     expect(tags.show('organizations', 123)).toEqual({
       method: 'GET',
-      url: '/api/v2/organizations/123/tags.json',
+      url: `https://${instance}.zendesk.com/api/v2/organizations/123/tags.json`,
       headers
     });
 
     expect(tags.show('users', 123)).toEqual({
       method: 'GET',
-      url: '/api/v2/users/123/tags.json',
+      url: `https://${instance}.zendesk.com/api/v2/users/123/tags.json`,
       headers
     });
   });
@@ -39,21 +40,21 @@ describe('Tags', () => {
   test('Set Tags', () => {
     expect(tags.set('tickets', 123, data)).toEqual({
       method: 'POST',
-      url: '/api/v2/tickets/123/tags.json',
+      url: `https://${instance}.zendesk.com/api/v2/tickets/123/tags.json`,
       headers,
       data
     });
 
     expect(tags.set('organizations', 123, data)).toEqual({
       method: 'POST',
-      url: '/api/v2/organizations/123/tags.json',
+      url: `https://${instance}.zendesk.com/api/v2/organizations/123/tags.json`,
       headers,
       data
     });
 
     expect(tags.set('users', 123, data)).toEqual({
       method: 'POST',
-      url: '/api/v2/users/123/tags.json',
+      url: `https://${instance}.zendesk.com/api/v2/users/123/tags.json`,
       headers,
       data
     });
@@ -62,21 +63,21 @@ describe('Tags', () => {
   test('Add Tags', () => {
     expect(tags.add('tickets', 123, data)).toEqual({
       method: 'PUT',
-      url: '/api/v2/tickets/123/tags.json',
+      url: `https://${instance}.zendesk.com/api/v2/tickets/123/tags.json`,
       headers,
       data
     });
 
     expect(tags.add('organizations', 123, data)).toEqual({
       method: 'PUT',
-      url: '/api/v2/organizations/123/tags.json',
+      url: `https://${instance}.zendesk.com/api/v2/organizations/123/tags.json`,
       headers,
       data
     });
 
     expect(tags.add('users', 123, data)).toEqual({
       method: 'PUT',
-      url: '/api/v2/users/123/tags.json',
+      url: `https://${instance}.zendesk.com/api/v2/users/123/tags.json`,
       headers,
       data
     });
@@ -85,21 +86,21 @@ describe('Tags', () => {
   test('Remove Tags', () => {
     expect(tags.remove('tickets', 123, data)).toEqual({
       method: 'DELETE',
-      url: '/api/v2/tickets/123/tags.json',
+      url: `https://${instance}.zendesk.com/api/v2/tickets/123/tags.json`,
       headers,
       data
     });
 
     expect(tags.remove('organizations', 123, data)).toEqual({
       method: 'DELETE',
-      url: '/api/v2/organizations/123/tags.json',
+      url: `https://${instance}.zendesk.com/api/v2/organizations/123/tags.json`,
       headers,
       data
     });
 
     expect(tags.remove('users', 123, data)).toEqual({
       method: 'DELETE',
-      url: '/api/v2/users/123/tags.json',
+      url: `https://${instance}.zendesk.com/api/v2/users/123/tags.json`,
       headers,
       data
     });
@@ -108,7 +109,7 @@ describe('Tags', () => {
   test('Autocomplete Tags', () => {
     expect(tags.autocomplete('tag_name')).toEqual({
       method: 'GET',
-      url: `/api/v2/autocomplete/tags.json?name=tag_name`,
+      url: `https://${instance}.zendesk.com/api/v2/autocomplete/tags.json?name=tag_name`,
       headers
     });
   });
