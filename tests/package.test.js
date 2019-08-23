b4e617684d1ac53e8105c1032db47282feb76f47
Andrew Milenic <androozka@gmail.com>
2019-08-22 23:53:54 -0700

    updated tests

--- tests/package.test.js
+++ tests/package.test.js
@@ -11,22 +11,3 @@ describe('Zendesk App Frameworks', () => {
     test('support api', () => expect(api.support).toBeDefined());
   });
 });
-
-// const zdApi = require('zendesk-api-js')
-// const { support } = new zdApi.v2(url, headers);
-// const { tags } = await support.tags.list();
-
-// const base64 = require('./node_modules/js-base64').Base64;
-
-// const api = require('zendesk-api-js').v2
-// const { support } = require('./src/api').v2;
-// const api = require('./index').v2();
-
-// console.log(api);
-
-// const email = '';
-// const token = '';
-// const headers = {
-//   'Content-Type': 'application/json',
-//   Authorization: `Basic ${base64.encode(`${email}/token:${token}`)}`
-// };
