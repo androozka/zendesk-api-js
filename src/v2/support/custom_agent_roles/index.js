bf05fc7a42b0cb28e6a5d9d819d6d3d09a8bab55
androozka <androozka@gmail.com>
2019-09-09 20:14:25 -0700

    built support:custom_agent_roles
    closes #20

---
+++ src/v2/support/custom_agent_roles/index.js
@@ -0,0 +1,67 @@
+const validate = require('./validate');
+
+module.exports = ({ instance, headers }) => {
+  const url = `https://${instance}.zendesk.com`;
+
+  return {
+    list: (options = null) => {
+      if (options) throw new Error('no options are allowed');
+
+      return {
+        method: 'GET',
+        url: `${url}/api/v2/custom_roles.json`,
+        headers
+      };
+    },
+
+    show: (options = {}) => {
+      const { error } = validate.show(options);
+      if (error) throw new Error(error.details[0].message);
+
+      const { id } = options;
+      return {
+        method: 'GET',
+        url: `${url}/api/v2/custom_roles/${id}.json`,
+        headers
+      };
+    },
+
+    create: (options = {}) => {
+      const { error } = validate.create(options);
+      if (error) throw new Error(error.details[0].message);
+
+      const { data } = options;
+      return {
+        method: 'POST',
+        url: `${url}/api/v2/custom_roles.json`,
+        headers,
+        data
+      };
+    },
+
+    update: (options = {}) => {
+      const { error } = validate.update(options);
+      if (error) throw new Error(error.details[0].message);
+
+      const { id, data } = options;
+      return {
+        method: 'PUT',
+        url: `${url}/api/v2/custom_roles/${id}.json`,
+        headers,
+        data
+      };
+    },
+
+    delete: (options = {}) => {
+      const { error } = validate.delete(options);
+      if (error) throw new Error(error.details[0].message);
+
+      const { id } = options;
+      return {
+        method: 'DELETE',
+        url: `${url}/api/v2/custom_roles/${id}.json`,
+        headers
+      };
+    }
+  };
+};
---
+++ src/v2/support/custom_agent_roles/validate.js
@@ -0,0 +1,13 @@
+const Joi = require('@hapi/joi');
+
+const id = Joi.number().min(1);
+const data = Joi.object();
+
+module.exports = {
+  list: null, // no options
+  show: options => Joi.validate(options, { id: id.required() }),
+  create: options => Joi.validate(options, { data: data.required() }),
+  update: options =>
+    Joi.validate(options, { id: id.required(), data: data.required() }),
+  delete: options => Joi.validate(options, { id: id.required() })
+};
