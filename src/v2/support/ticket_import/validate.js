8f65831fbd47e7bc573323afdd507c1844c16d3b
androozka <androozka@gmail.com>
2019-09-18 09:02:01 -0700

    fixed ticket_import

--- src/v2/support/ticket_import/validate.js
+++ src/v2/support/ticket_import/validate.js
@@ -3,6 +3,13 @@ const Joi = require('@hapi/joi');
 const data = Joi.object().min(1);
 
 module.exports = {
-  single: options => Joi.validate(options, { data: data.required() }),
-  bulk: options => Joi.validate(options, { data: data.required() })
+  single: options =>
+    Joi.object({
+      data: data.required()
+    }).validate(options),
+
+  bulk: options =>
+    Joi.object({
+      data: data.required()
+    }).validate(options)
 };
