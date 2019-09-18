11912b4a0346a4b8dde7b4a605a9bd04bf75f6db
androozka <androozka@gmail.com>
2019-09-18 09:20:09 -0700

    fixed search

--- src/v2/support/search/validate.js
+++ src/v2/support/search/validate.js
@@ -3,4 +3,6 @@ const Joi = require('@hapi/joi');
 const search_string = Joi.string().min(1);
 
 module.exports = options =>
-  Joi.validate(options, { search_string: search_string.required() });
+  Joi.object({
+    search_string: search_string.required()
+  }).validate(options);
