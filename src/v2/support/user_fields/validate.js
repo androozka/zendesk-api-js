759a833338b8cab312566bfca835f72831462ee6
androozka <androozka@gmail.com>
2019-09-18 08:48:33 -0700

    fixed user_fields

--- src/v2/support/user_fields/validate.js
+++ src/v2/support/user_fields/validate.js
@@ -6,21 +6,53 @@ const data = Joi.object();
 
 module.exports = {
   list: null, // no options
-  show: options => Joi.validate(options, { id: id.required() }),
-  create: options => Joi.validate(options, { data: data.required() }),
+
+  show: options =>
+    Joi.object({
+      id: id.required()
+    }).validate(options),
+
+  create: options =>
+    Joi.object({
+      data: data.required()
+    }).validate(options),
+
   update: options =>
-    Joi.validate(options, { id: id.required(), data: data.required() }),
-  delete: options => Joi.validate(options, { id: id.required() }),
-  reorder: options => Joi.validate(options, { data: data.required() }),
+    Joi.object({
+      id: id.required(),
+      data: data.required()
+    }).validate(options),
+
+  delete: options =>
+    Joi.object({
+      id: id.required()
+    }).validate(options),
+
+  reorder: options =>
+    Joi.object({
+      data: data.required()
+    }).validate(options),
+
   listOptions: options =>
-    Joi.validate(options, { field_id: field_id.required() }),
+    Joi.object({
+      field_id: field_id.required()
+    }).validate(options),
+
   showOption: options =>
-    Joi.validate(options, { field_id: field_id.required(), id: id.required() }),
+    Joi.object({
+      field_id: field_id.required(),
+      id: id.required()
+    }).validate(options),
+
   createOrUpdateOption: options =>
-    Joi.validate(options, {
+    Joi.object({
       field_id: field_id.required(),
       data: data.required()
-    }),
+    }).validate(options),
+
   deleteOption: options =>
-    Joi.validate(options, { field_id: field_id.required(), id: id.required() })
+    Joi.object({
+      field_id: field_id.required(),
+      id: id.required()
+    }).validate(options)
 };
