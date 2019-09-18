5e8e61ec8573d64b5064a130a550894d51f5dc17
androozka <androozka@gmail.com>
2019-09-18 09:44:58 -0700

    fixed custom_agent_roles

--- src/v2/support/custom_agent_roles/validate.js
+++ src/v2/support/custom_agent_roles/validate.js
@@ -5,9 +5,25 @@ const data = Joi.object();
 
 module.exports = {
   list: null, // no options
-  show: options => Joi.validate(options, { id: id.required() }),
-  create: options => Joi.validate(options, { data: data.required() }),
+
+  show: options =>
+    Joi.object({
+      id: id.required()
+    }).validate(options),
+
+  create: options =>
+    Joi.object({
+      data: data.required()
+    }).validate(options),
+
   update: options =>
-    Joi.validate(options, { id: id.required(), data: data.required() }),
-  delete: options => Joi.validate(options, { id: id.required() })
+    Joi.object({
+      id: id.required(),
+      data: data.required()
+    }).validate(options),
+
+  delete: options =>
+    Joi.object({
+      id: id.required()
+    }).validate(options)
 };
